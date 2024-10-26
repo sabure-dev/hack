f81f04e438678ff7016258f016f222e1a513cf5b
fleron19 <romanovskymv@gmial.com>
2024-10-26 16:18:11 +0300

    add theme change

--- frontend/main.py
+++ frontend/main.py
@@ -2,11 +2,12 @@ import flet as ft
 
 
 def main(page):
-    page.title = "Вход"
+    page.title = "Вход в Hackwars"
     page.theme_mode = 'light'
+    page.route = "/login"
     page.window.title_bar_hidden = False
     page.window.frameless = False
-    page.window.height = 540
+    page.window.height = 620
     page.window.width = 960
     page.window.resizable = False
 
@@ -25,43 +26,117 @@ def main(page):
         )
         page.update()
 
-    def btn_sign_in():
+    def btn_alredy_have(e):
+        page.route = "/login"
         page.update()
         login.current.focus()
 
-    def btn_sign_up():
+    def btn_havent(e):
+        page.route = "/register"
+        page.update()
+        login.current.focus()
+
+    def btn_sign_in(e):
+        page.update()
+        login.current.focus()
+
+    def btn_sign_up(e):
         page.update()
         login.current.focus()
 
     c = ft.Switch(label="Светлая тема", on_change=theme_changed)
-
+    hackwars = ft.Row([ft.Text("Hackwars", size=64, font_family="Bauhaus LT(RUS BY LYAJKA)")],
+                               alignment=ft.MainAxisAlignment.CENTER)
     enter_row = ft.Row(
         [ft.ElevatedButton("Войти", on_click=btn_sign_in)],
         width=300,
         alignment=ft.MainAxisAlignment.END,
 
     )
-    page.add(
-        ft.Container(
-            ft.Row(
-                [c],
-                alignment=ft.MainAxisAlignment.START,
-            )
+    reg_row = ft.Row(
+        [ft.ElevatedButton("Зарегистрироваться", on_click=btn_sign_up)],
+        width=300,
+        alignment=ft.MainAxisAlignment.END,
 
-        ),
-        ft.Container(
-            ft.Column(
-                [ft.Text("Hackhatons", size=32, font_family="Bauhaus LT(RUS BY LYAJKA)",), ft.TextField(ref=login, label="Логин", autofocus=True, width=300),
-                 ft.TextField(ref=password, label="Пароль", password=True, can_reveal_password=True, width=300),
-                 enter_row,
-                 ft.Row([ft.Text("Нет аккаунта?", size=14)], width=300, alignment=ft.MainAxisAlignment.CENTER),
-                 ft.Row([ft.ElevatedButton("Зарегистрироваться", on_click=btn_sign_up)], width=300, alignment=ft.MainAxisAlignment.CENTER)],
-                height=405,
-                alignment=ft.MainAxisAlignment.CENTER,
-            ),
-            alignment=ft.alignment.center,
-        )
     )
+    page.add(
+
+    )
+
+    def route_change(route):
+        page.views.clear()
+        page.title = "Вход в Hackwars"
+        page.views.append(
+            ft.View(
+                "/login",
+                [
+                    ft.Container(
+                        ft.Row(
+                            [c],
+                            alignment=ft.MainAxisAlignment.START,
+                        )
+
+                    ),
+                    hackwars,
+                    ft.Container(
+                        ft.Column(
+                            [
+                             ft.Text("Вход", size=28, font_family="Bauhaus LT(RUS BY LYAJKA)"),
+                             ft.TextField(ref=login, label="Логин", autofocus=True, width=300),
+                             ft.TextField(ref=password, label="Пароль", password=True, can_reveal_password=True,
+                                          width=300),
+                             enter_row,
+                             ft.Row([ft.Text("Нет аккаунта?", size=14)], width=300,
+                                    alignment=ft.MainAxisAlignment.CENTER),
+                             ft.Row([ft.ElevatedButton("Зарегистрироваться", on_click=btn_havent)], width=300,
+                                    alignment=ft.MainAxisAlignment.CENTER)],
+                            height=405,
+                            alignment=ft.MainAxisAlignment.CENTER,
+                        ),
+                        alignment=ft.alignment.center,
+                    ),
+                ],
+            )
+        )
+        if page.route == '/register':
+            page.title = "Регистрация в Hackwars"
+            page.views.append(
+                ft.View(
+                    "/register",
+                    [
+                        ft.Container(
+                            ft.Row(
+                                [c],
+                                alignment=ft.MainAxisAlignment.START,
+                            )
+
+                        ),
+                        hackwars,
+                        ft.Container(
+                            ft.Column(
+                                [ft.Text("Регистрация", size=32, font_family="Bauhaus LT(RUS BY LYAJKA)", ),
+                                 ft.TextField(ref=login, label="Логин", autofocus=True, width=300),
+                                 ft.TextField(ref=password, label="Пароль", password=True, can_reveal_password=True,
+                                              width=300),
+                                 ft.TextField(ref=password, label="Повторите пароль", password=True, can_reveal_password=True,
+                                              width=300),
+                                 reg_row,
+                                 ft.Row([ft.Text("Есть аккаунт?", size=14)], width=300,
+                                        alignment=ft.MainAxisAlignment.CENTER),
+                                 ft.Row([ft.ElevatedButton("Войти", on_click=btn_alredy_have)], width=300,
+                                        alignment=ft.MainAxisAlignment.CENTER)],
+                                height=405,
+                                alignment=ft.MainAxisAlignment.CENTER,
+                            ),
+                            alignment=ft.alignment.center,
+                        ),
+                    ],
+                )
+            )
+        page.update()
+
+    page.on_route_change = route_change
+    page.go(page.route)
 
 
 ft.app(target=main)
